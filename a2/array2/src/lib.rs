17c79448dad0e5657d435a1cf5f40612d5e3c6ca
Matt Boekamp <matthew_boekamp@uri.edu>
2024-02-13 14:39:45 -0500

    add lab folder, file for a2

--- a2/array2/src/lib.rs
+++ a2/array2/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Array2<T: Clone> {
+    width: usize,
+    height: usize,
+    elements: Vec<T>, // 1D array to store elements
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T: Clone> Array2<T> {
+    // Creates a new Array with uninitialized elements.
+    pub fn new(width: usize, height: usize) -> Self {
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // Constructs an Array in column-major
+    pub fn from_col_major(elements: Vec<T>, width: usize, height: usize) -> Self {
+    }
+
+    // Constructs an Array in row-major 
+    pub fn from_row_major(elements: Vec<T>, width: usize, height: usize) -> Self {
+    }
+
+    // Iterator over the elements in row-major order.
+    pub fn iter_row_major(&self) -> impl Iterator<Item = &T> {
+    }
+
+    // Iterator over the elements in column-major order.
+    pub fn iter_col_major(&self) -> impl Iterator<Item = &T> {
+    }
+
+    // Finds an element in the array and returns its position.
+    pub fn find_element(&self, target: &T) -> Option<(usize, usize)> {
     }
 }
